e30d8d6e18c2ab05a470ea5e7362bb5d32b66de4
Ong Wen Pin <wenpin2108@gmail.com>
2024-07-29 12:35:25 +0800

    Add modularity to delete dialog in list pages

--- src/pages/ExpenseList.jsx
+++ src/pages/ExpenseList.jsx
@@ -8,6 +8,7 @@ import { useSelector } from 'react-redux';
 import Searchbar from '../components/Searchbar';
 import { getAccessToken } from '../api/authApi';
 import { getExpenses, deleteExpense } from '../api/expenseApi';
+import DeleteDialog from '../components/DeleteDialog';
 
 export default function ExpenseList() {
 
@@ -167,78 +168,15 @@ export default function ExpenseList() {
     return (
         <>
             <>
-                <Transition show={displayExpenseDeleteDialog}>
-                    <Dialog className="relative z-10" onClose={setDisplayExpenseDeleteDialog}>
-                        <TransitionChild
-                        enter="ease-out duration-300"
-                        enterFrom="opacity-0"
-                        enterTo="opacity-100"
-                        leave="ease-in duration-200"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
-                        >
-                            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-                        </TransitionChild>
-
-                        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-                            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-                                <TransitionChild
-                                enter="ease-out duration-300"
-                                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                                enterTo="opacity-100 translate-y-0 sm:scale-100"
-                                leave="ease-in duration-200"
-                                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                                >
-                                    <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                                        <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                                            <div className="sm:flex sm:items-start">
-                                                <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                                                    <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
-                                                </div>
-                                                <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                                                    <DialogTitle as="h3" className="text-base font-semibold leading-6 text-gray-900">
-                                                        Remove {selectedExpense && selectedExpense._id} from records
-                                                    </DialogTitle>
-                                                <div className="mt-2">
-                                                    <p className="text-sm text-gray-500">
-                                                    Are you sure you want to remove {selectedExpense && selectedExpense._id}? All of the data in the record will be permanently removed. This action cannot be undone.
-                                                    </p>
-                                                </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                                        <button
-                                            type="button"
-                                            className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
-                                            onClick={() => {
-                                                handleDeleteExpense(selectedExpense._id).then(() => {
-                                                    setSelectedExpense(null);
-                                                    setDisplayExpenseDeleteDialog(false);
-                                                });
-                                            }}
-                                        >
-                                            Remove
-                                        </button>
-                                        <button
-                                            type="button"
-                                            className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                                            onClick={() => {
-                                                setSelectedExpense(null);
-                                                setDisplayExpenseDeleteDialog(false);
-                                            }}
-                                            data-autofocus
-                                        >
-                                            Cancel
-                                        </button>
-                                        </div>
-                                    </DialogPanel>
-                                </TransitionChild>
-                            </div>
-                        </div>
-                    </Dialog>
-                </Transition>
+                <DeleteDialog
+                    displayDialog={displayExpenseDeleteDialog}
+                    setDisplayDialog={setDisplayExpenseDeleteDialog}
+                    title={`Remove ${selectedExpense && selectedExpense._id} from expenses`}
+                    message={`Are you sure you want to remove ${selectedExpense && selectedExpense._id}? All of the data in the record will be permanently removed. This action cannot be undone.`}
+                    setSelected={setSelectedExpense}
+                    handleDelete={handleDeleteExpense}
+                    deleteTarget={selectedExpense && selectedExpense._id}
+                />
             </>
             
             <>
